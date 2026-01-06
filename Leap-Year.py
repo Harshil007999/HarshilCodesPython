a7b1a55f098c8f91baed8ba92a3f21f6839ea07c
Harshil007999 <124021201026@adit.ac.in>
2026-01-07 01:11:51 +0530

    Fixed Code

--- Leap-Year.py
+++ Leap-Year.py
@@ -1,5 +1,5 @@
 x = int(input("Enter the Year: "))
-if (x % 4 == 0 and x % 100 != 0):
+if (x % 4 == 0 and x % 100 != 0) or x % 400 == 0:
     print(f"The Year {x} is a Leap Year")
 else:
     print(f"The Year {x} is Not a Leap Year")
